b8eae1036b722392ab06ee5551c63ff066690301
Svatoi <privitvlad00@gmail.com>
2026-05-10 16:23:19 +0200

    public xd

--- src/main/java/net/egrasel/tutorialmod/TutorialMod.java
+++ src/main/java/net/egrasel/tutorialmod/TutorialMod.java
@@ -18,10 +18,8 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MODID)
 public class TutorialMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod(FMLJavaModLoadingContext context)
     {
