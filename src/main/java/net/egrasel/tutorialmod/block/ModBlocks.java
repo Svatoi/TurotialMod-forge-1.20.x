0c9ddf2e80492bdf42d9ca62eb8b4eab9e530449
Svatoi <privitvlad00@gmail.com>
2026-05-17 15:36:40 +0200

    add sound block

--- src/main/java/net/egrasel/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/egrasel/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.egrasel.tutorialmod.block;
 
 import net.egrasel.tutorialmod.TutorialMod;
+import net.egrasel.tutorialmod.block.custom.SoundBlock;
 import net.egrasel.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -38,6 +39,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE).strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 6)
             ));
+
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/egrasel/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/egrasel/tutorialmod/item/ModCreativeModTabs.java
@@ -31,6 +31,9 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
+
                     })
                     .build());
 
